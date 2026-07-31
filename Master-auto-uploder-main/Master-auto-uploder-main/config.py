a1b14883357e576477defc096298ff611d5bdef7
sahilupta <guptasahil81660@gmail.com>
2026-07-31 06:09:13 +0530

    Update config.py

--- Master-auto-uploder-main/Master-auto-uploder-main/config.py
+++ Master-auto-uploder-main/Master-auto-uploder-main/config.py
@@ -3,12 +3,12 @@ import os
 ##Code Written By ꧁༒☬𝗦AHIL DEVLOPER☬༒꧂
 
 class Config(object):
-    BOT_TOKEN = "8525803667:AAEjpylk_qaZ62O_gIZmch46S8VQNYxcY1M"
-    DB_NAME = "nattu"
-    API_ID = "6886135"
-    API_HASH = "ee20a1c8a8e44eaa638b7254cbcc3012"
-    ADMIN_ID = 8532700793
-    DB_URL = "mongodb+srv://nattu:nattu@cluster0.quvds.mongodb.net/?appName=Cluster0"
-    LOG_CHANNEL = "-1003873871474" # Your Log Channel ID (Bot ko ADMIN BNAYE)
-    USERLINK = "https://t.me/Natking_uploader_bot"
-    TUTORIAL_VIDEO = "https://t.me/Natking_uploader_bot"
+    BOT_TOKEN = "8842602749:AAECqZU_dC4mdqrymIb7cmA8HZqylD144tM"
+    DB_NAME = "master_txt_to_video_auto_uploader"
+    API_ID = "28221704"
+    API_HASH = "a0163f47bc5262a3106b7858720a09fb"
+    ADMIN_ID = 8321801403
+    DB_URL = "mongodb+srv://sg4828990_db_user:<YOUR_MONGODB_PASSWORD>@cluster0.2brxvvv.mongodb.net/master_txt_to_video_auto_uploader?retryWrites=true&w=majority&appName=Cluster0"
+    LOG_CHANNEL = "-1004374834303" # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    USERLINK = "http://t.me/masterautouploader_bot"
+    
