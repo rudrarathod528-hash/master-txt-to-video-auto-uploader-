f1b4c7080115fdcd819bb11fe9ec1a49738844f7
rudrarathod528-hash <rudrarathod528@gmail.com>
2026-08-14 18:57:40 +0530

    Update config.py

--- Master-auto-uploder-main/Master-auto-uploder-main/config.py
+++ Master-auto-uploder-main/Master-auto-uploder-main/config.py
@@ -4,11 +4,11 @@ import os
 
 class Config(object):
     BOT_TOKEN = "8628214594:AAHFI6rMNVfEv3-u8wvpKsegSvZGdvet-aI"
-    DB_NAME = "master_txt_to_video_auto_uploader"
+    DB_NAME = ""
     API_ID = "33088642"
     API_HASH = "bf6a7d6071350cb64849d46b8b4849e9"
     ADMIN_ID = 5808599565
-    DB_URL = "mongodb+srv://sg4828990_db_user:<YOUR_MONGODB_PASSWORD>@cluster0.2brxvvv.mongodb.net/master_txt_to_video_auto_uploader?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://Rruutt:rudra@123@cluster0.41t7ev6.mongodb.net/?appName=Cluster0"
     LOG_CHANNEL = "-1003823684134" # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = ""
     
