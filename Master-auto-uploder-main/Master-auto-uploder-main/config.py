1102e6ed501e4ee2b656dd8ee5c33f7549268925
rudrarathod528-hash <rudrarathod528@gmail.com>
2026-08-14 19:00:14 +0530

    Update config.py

--- Master-auto-uploder-main/Master-auto-uploder-main/config.py
+++ Master-auto-uploder-main/Master-auto-uploder-main/config.py
@@ -1,1 +1,14 @@
+import os
+##Code Written By ꧁༒☬𝗦AHIL DEVLOPER☬༒꧂
+##Code Written By ꧁༒☬𝗦AHIL DEVLOPER☬༒꧂
 
+class Config(object):
+    BOT_TOKEN = "8628214594:AAHFI6rMNVfEv3-u8wvpKsegSvZGdvet-aI"
+    DB_NAME = ""
+    API_ID = "33088642"
+    API_HASH = "bf6a7d6071350cb64849d46b8b4849e9"
+    ADMIN_ID = 5808599565
+    DB_URL = "mongodb+srv://Rruutt:rudra@123@cluster0.41t7ev6.mongodb.net/?appName=Cluster0"
+    LOG_CHANNEL = "-1003823684134" # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    USERLINK = ""
+    
